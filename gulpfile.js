a0a651992487ec9c91dc679db5de1dae46f30a5d
Ovidio R. Calvet <calveto@gmail.com>
2016-01-08 22:33:59 -0500

    Added livereload and compilation of css files

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,35 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
+ var less = require('gulp-less');
  
  // Setup a webserver
 gulp.task('webserver', function() {
   var serverPort = process.env.FS_PORT || 8088;
   connect.server({
-    port: serverPort
+    port: serverPort,
+    root: '.tmp',
+    fallback: 'index.html',
+    livereload: true
   });
   console.log('Webserver started on port: ' + serverPort);
 });
 
-gulp.task('default', ['webserver']);
+gulp.task('less', function() {
+  gulp.src('styles/main.less')
+    .pipe(less())
+    .pipe(gulp.dest('.tmp/styles'))
+    .pipe(connect.reload());
+});
+
+gulp.task('tmp', function() {
+  gulp.src('index.html')
+    .pipe(gulp.dest('.tmp/index.html'))
+    .pipe(connect.reload());
+})
+ 
+gulp.task('watch', function() {
+    gulp.watch('styles/*.less', ['less']);
+    gulp.watch('index.html', ['tmp']);
+})
+ 
+gulp.task('default', ['less', 'tmp', 'webserver', 'watch']);
