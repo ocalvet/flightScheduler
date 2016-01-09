d895203e375d4c86c1b58669c12f6b0b0fb1b692
Ovidio R. Calvet <calveto@gmail.com>
2016-01-09 18:32:51 -0500

    Removed .tmp folder

--- .tmp/app/flightScheduler.js
+++
@@ -1,9 +0,0 @@
-(function(module) {
-  
-  module('flightScheduler', [])
-    .controller('AppCtrl', [function() {
-      var appCtrl = this;
-      appCtrl.title = "Hello from angular";
-    }]);
-  
-})(angular.module);
