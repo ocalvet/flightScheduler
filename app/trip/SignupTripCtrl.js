b98a9594c856df1c4b3829aa4144ef5e537ca4be
Yohay Berger <yohay@hotmail.com>
2016-01-16 19:28:59 -0500

    Added signup information

--- app/trip/SignupTripCtrl.js
+++ app/trip/SignupTripCtrl.js
@@ -1,9 +1,17 @@
 (function(module) {
   
   module('flightScheduler')
-    .controller('SignupTripCtrl', ['$stateParams', function($stateParams) {
+    .controller('SignupTripCtrl', ['$stateParams', 'tripService', function($stateParams,tripService) {
       var signupTripCtrl = this;
-      signupTripCtrl.title = "Trip Signup";
+      signupTripCtrl.title = "Sign up for a trip with ";
+      
+      tripService.getTripDetails($stateParams.tripId)
+        .then(function(tripDetails) {
+          signupTripCtrl.tripDetails = tripDetails;          
+        }, function (err) {
+          console.log('There was an error getting the upcoming trips', err);
+        });
+      
       console.log('Trip id', $stateParams.tripId);
     }]);
   
