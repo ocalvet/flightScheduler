ee396dad184890a0b4e15992ebda9acacb3712a7
Ovidio R. Calvet <calveto@gmail.com>
2016-01-17 19:54:30 -0500

    Added a fake test

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var should = require('should');
+
+describe('Sample', function() {
+  describe('test', function() {
+    it('should pass', function() {
+       true.should.be.true;
+    });
+  });
+});
