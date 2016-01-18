cd677a0a178fd04c27b6ba7fee8927e42086f742
Ovidio R. Calvet <calveto@gmail.com>
2016-01-17 20:00:57 -0500

    Added a scheduleService

---
+++ app/common/scheduleService.js
@@ -0,0 +1,19 @@
+(function(module) {
+  
+  module('flightScheduler')
+    .service('scheduleService', ['$q', function($q) {
+      
+      var getAvailableDestinationDates = function(destinationId) {
+        var defer = $q.defer();
+        
+        defer.resolve({});
+        
+        return defer.promise;
+      };
+      
+      return {
+        getAvailability: getAvailableDestinationDates
+      }
+    }])
+  
+})(angular.module)
