1fab01c9189982d577cd3b3dbee1325618d2c946
Ovidio R. Calvet <calveto@gmail.com>
2016-02-25 00:36:53 -0500

    Added support on the dashboard for the ui.router

--- app/dashboard/yogoTravelDashboard.js
+++ app/dashboard/yogoTravelDashboard.js
@@ -1,10 +1,25 @@
 (function(module) {
-    
-    module('yogoTravelDashboard', ['ngMaterial', 'draganddrop', 'ngLodash', 'common'])
+
+    module('yogoTravelDashboard', ['ngMaterial', 'draganddrop', 'ngLodash', 'common','ui.router'])
       .config(function($mdThemingProvider) {
         $mdThemingProvider.theme('default')
           .primaryPalette('teal')
           .accentPalette('orange');
-      });
-      
+      })
+    .config(function($stateProvider, $urlRouterProvider) {
+
+      // For any unmatched url, redirect to /state1
+      $urlRouterProvider.otherwise("/dash");
+
+      // Set up the application states
+      $stateProvider
+        .state('dash', {
+          url: '/dash',
+          templateUrl: 'views/dash.html',
+          controller: 'DashboardAppCtrl as da'
+        })
+   });
+
+
+
 })(angular.module);
--- app/dashboard/ytCreateTrip.js
+++ app/dashboard/ytCreateTrip.js
@@ -1,24 +1,24 @@
 (function(module) {
-    
+
     module('yogoTravelDashboard')
         .directive('ytCreateTrip', [function(){
-            
+
             return {
               restrict: 'E',
               scope: {
-                currentUser: '=user'  
+                currentUser: '=user'
               },
               templateUrl: 'views/ytCreateTrip.html',
               link: function(scope, el, attr) {
                   scope.$watch('currentUser', function(user) {
                       if(user) {
                         console.log('user ', scope.currentUser);
-                      } 
+                      }
                   });
               },
               controller: "CreateTripCtrl as ct"
             };
-            
+
         }]);
-    
+
 })(angular.module)
